2f5c6ab69bf48eef040d8586ab2d1c40486a52b4
Prince Bhardwaj <53052669+princebhardwaj9@users.noreply.github.com>
2021-02-01 17:50:47 +0530

    Update canary_shopify_cart.js

--- canary_shopify_cart.js
+++ canary_shopify_cart.js
@@ -1,47 +1,67 @@
-window.addEventListener('message', function(eventData) {
-    try { 
+window.addEventListener('message', function (eventData) {
+    try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-             if (event.event_code === "custom-event" && event.data && event.data.code === "add-cart") {
+            if (event.event_code === "custom-event" && event.data && event.data.code === "add-cart") {
                 jQuery.post('/cart/add.js', {
                     items: event.data.data
                 });
                 return;
-            }
-            else if (event.event_code === "custom-event" && event.data && event.data.code === "update-cart") {
+            } else if (event.event_code === "custom-event" && event.data && event.data.code === "update-cart") {
                 jQuery.post('/cart/update.js', {
-                  updates:event.data.data
+                    updates: event.data.data
                 });
                 return;
-            }
-            else if (event.event_code === "custom-event" && event.data && event.data.code === "clear-cart") {
+            } else if (event.event_code === "custom-event" && event.data && event.data.code === "clear-cart") {
                 jQuery.post('/cart/clear.js');
                 return;
-            }
-            else if (event.event_code === "custom-event" && event.data && event.data.code === "get-cart") {
-               jQuery.getJSON('/cart.js', function(cart) {                   
-                   if (cart && cart.items && cart.items.length > 0) {
-                      document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
-                             event_code: 'ym-client-event',
-                             data: JSON.stringify({
-                             event: {
-                                  code: "get-cart-event",
-                                  data: cart
-                                 }
-                             })
+            } else if (event.event_code === "custom-event" && event.data && event.data.code === "get-cart") {
+                jQuery.getJSON('/cart.js', function (cart) {
+                    if (cart && cart.items && cart.items.length > 0) {
+                        document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+                            event_code: 'ym-client-event',
+                            data: JSON.stringify({
+                                event: {
+                                    code: "get-cart-event",
+                                    data: cart
+                                }
+                            })
                         }), '*');
-                   }
-               });
+                    }
+                });
+                return;
+            } else if (event.event_code === "custom-event" && event.data && event.data.code === "product-recomend") {
+                let pid = event.data.pid;
+                jQuery.getJSON(`/recommendations/products.json?product_id=${pid}&limit=4`, function (
+                    response
+                ) {
+                    var recommendedProducts = response.products;
+                    if (recommendedProducts.length > 0) {
+                        // var firstRecommendedProduct = recommendedProducts[0];
+                        // alert(
+                        //     "The title of the first recommended product is: " +
+                        //     firstRecommendedProduct.title
+                        // );
+
+                        document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+                            event_code: 'ym-recomend-event',
+                            data: JSON.stringify({
+                                event: {
+                                    code: "product-recomend-event",
+                                    data: recommendedProducts
+                                }
+                            })
+                        }), '*');
+
+                    }
+                });
+            } else if (event.event_code === "custom-event" && event.data && event.data.code === "page_open") {
+                var newWindow = window.open(event.data.data, "_self");
+                return;
+            } else {
                 return;
             }
-            else if (event.event_code === "custom-event" && event.data && event.data.code === "page_open") {
-                var newWindow = window.open(event.data.data,"_self");
-                return;
-            }
-            else{
-                return;
-            }
-         }
+        }
     } catch (error) {
         return;
     }

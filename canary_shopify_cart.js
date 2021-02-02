87684f69d817717a51681887d096f1ac7eac11b9
Prince Bhardwaj <53052669+princebhardwaj9@users.noreply.github.com>
2021-02-02 13:36:38 +0530

    Update canary_shopify_cart.js

--- canary_shopify_cart.js
+++ canary_shopify_cart.js
@@ -1,4 +1,5 @@
 window.addEventListener('message', function (eventData) {
+    app.sendTextMessage('In Script');
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
